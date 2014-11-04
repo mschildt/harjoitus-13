ca23be11e6bf1384fbd2c95341b3a5ee33295fcb
mschildt <mikaelschildt@gmail.com>
2014-11-04 11:42:41 +0200

    harj13

---
+++ harj13/Source.cpp
@@ -0,0 +1,49 @@
+/*	Työn nimi: Harjoitus 13
+	Tekijän nimi: Mikael Schildt
+
+	Työn kuvaus:
+
+	Tee ohjelma, joka kysyy etunimesi ja sukunimesi. Ohjelma
+	yhdistää nimet yhdeksi merkkijonoksi ja tulostaa ne
+	lopuksi naytölle.
+
+
+	a) Kayta cstring-kirjastoa (C:n merkkitaulukot)
+	tehdäksesi merkkijonojen yhdistämisen
+	b) Kayta string-kirjastoa (C++:n merkkijonot)
+	tehdäksesi merkkijonojen yhdistämisen  TEE, JOS ON AIKAA
+
+	Esimerkki
+	Anna etunimi: Aku
+	Anna sukunimi: Ankka
+	Nimesi oli: Aku Ankka
+
+*/
+
+#include <iostream>
+using namespace std;
+
+#include <cstring>
+
+
+int main()
+{
+	char etunimi[30];
+	char sukunimi[30];
+	char vali[2] = " ";
+
+
+	cout << "Anna etunimi: ";
+	cin >> etunimi;
+	cout << "Anna sukunimi: ";
+	cin >> sukunimi;
+
+	strcat_s(etunimi, vali);
+	strcat_s(etunimi, sukunimi);
+	cout << "Nimesi oli: " << etunimi << endl;
+
+
+
+
+
+}
